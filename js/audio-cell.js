0a8c8f4c29f49b3b4c3255b03e216666092dc5ac
Jaakko Rinta-Filppula <jaakko.rf@gmail.com>
2014-09-04 19:47:01 +0300

    initial commit

---
+++ js/audio-cell.js
@@ -0,0 +1,42 @@
+(function() {
+    'use strict';
+    
+    var AudioCell = function(ctx, masterGain, frequency, onended) {
+        this.audioCtx = ctx;
+        
+        this.osc = ctx.createOscillator();
+        this.gain = ctx.createGain();
+        
+        this.osc.frequency.value = frequency;
+        this.osc.connect(this.gain);
+        
+        this.gain.gain.value = 0.0;
+        this.gain.connect(masterGain);
+        
+        this.frequency = frequency;
+        
+        this.onended = onended;
+        
+        this.osc.start();
+    };
+    
+    AudioCell.prototype.start = function(delay) {
+        var that = this;
+        setTimeout(function() {
+            that.gain.gain.value = 1.0;
+        }, delay * 1000);
+    };
+    
+    AudioCell.prototype.stop = function(delay) {
+        var that = this;
+        setTimeout(function() {
+            that.gain.gain.value = 0.0;
+            
+            if (that.onended) {
+                that.onended();
+            }
+        }, delay * 1000);
+    };
+    
+    window.AudioCell = AudioCell;
+})();
---
+++ js/controllers/main.js
@@ -0,0 +1,85 @@
+(function() {
+    'use strict';
+    
+    angular.module('sequencer')
+        .controller('MainCtrl', MainCtrl);
+        
+    /* @ngInject */
+    function MainCtrl($interval) {
+        var audioCtx = new AudioContext();
+        
+        var vm = this;
+        
+        vm.bpm = 160;
+        vm.beatDuration = 60 / vm.bpm;
+        
+        vm.beats = 16;
+        
+        vm.currentBeat = 0;
+        
+        vm.audioCells = [];
+        
+        vm.scale = [
+            {freq: 440, index: 0},
+            {freq: 493.88, index: 1},
+            {freq: 523.25, index: 2},
+            {freq: 587.33, index: 3},
+            {freq: 659.26, index: 4},
+            {freq: 698.46, index: 5},
+            {freq: 783.99, index: 6},
+            {freq: 880, index: 7}
+        ];
+        
+        vm.masterVolume = audioCtx.createGain();
+        vm.masterVolume.gain.value = 0.2;
+        vm.masterVolume.connect(audioCtx.destination);
+        
+        vm.getBeats = function() {
+            return new Array(vm.beats);
+        };
+        
+        vm.addAudioCell = function(beat, note) {
+            console.log(beat);
+            if (!vm.audioCells[beat]) {
+                vm.audioCells[beat] = [];
+            }
+            
+            var newCell = new AudioCell(audioCtx, vm.masterVolume, note.freq);
+            vm.audioCells[beat][note.index] = newCell;
+        };
+        
+        vm.getAudioCell = function(beat, note) {
+            if (!vm.audioCells[beat]) {
+                return null;
+            }
+            
+            return vm.audioCells[beat][note.index] ? vm.audioCells[beat][note.index] : null;
+        };
+        
+        playAudioCells(vm.currentBeat);
+        var lastUpdate = 0;
+        $interval(function() {
+            vm.currentBeat = (vm.currentBeat + 1);
+            
+            if (vm.currentBeat >= vm.beats) {
+                vm.currentBeat = 0;
+            }
+            playAudioCells(vm.currentBeat);
+            
+            var now = window.performance.now();
+            console.log(now - lastUpdate);
+            lastUpdate = now;
+        }, vm.beatDuration * 1000);
+        
+        function playAudioCells(beat) {
+            if (!vm.audioCells[beat]) {
+                return;
+            }
+            
+            vm.audioCells[beat].forEach(function(cell) {
+                cell.start();
+                cell.stop(vm.beatDuration);
+            });
+        }
+    }
+})();
